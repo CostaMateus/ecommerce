b8b59dfda13d1ec25aa694b8143d8ed22466b3bf
Mateus Costa <costa.mack95@gmail.com>
2018-01-27 21:13:06 -0200

    Admin - categorias

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -324,6 +325,127 @@ $app->post('/admin/forgot/reset' , function() {
 
 });
 
+/**
+ * Rota da página que lista as categorias 
+ * @param type '/admin/categories' 
+ * @param type function( 
+ * @return type
+ */
+$app->get('/admin/categories', function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+});
+
+/**
+ * Rota da página que adiciona nova categoria
+ * @param type '/admin/categories/create' 
+ * @param type function( 
+ * @return type
+ */
+$app->get('/admin/categories/create', function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+/**
+ * Rota _POST da página que adiciona nova categoria, insere no banco
+ * @param type '/admin/categories/create' 
+ * @param type function( 
+ * @return type
+ */
+$app->post('/admin/categories/create', function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+/**
+ * Rota _POST da página que apaga uma categoria, deleta do banco
+ * @param type '/admin/categories/:idcategory/delete' 
+ * @param type function($idcategory 
+ * @return type
+ */
+$app->get('/admin/categories/:idcategory/delete', function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+/**
+ * Rota da página que edita uma categoria
+ * @param type '/admin/categories/:idcategory' 
+ * @param type function($idcategory 
+ * @return type
+ */
+$app->get('/admin/categories/:idcategory', function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+/**
+ * Rota _POST da página que edita uma categoria, atualiza no banco
+ * @param type '/admin/categories/:idcategory' 
+ * @param type function($idcategory 
+ * @return type
+ */
+$app->post('/admin/categories/:idcategory', function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
+
+class Category extends Model 
+{
+
+	/**
+	 * Busca no banco todos os registros de categorias
+	 * @return type
+	 */
+	public static function listAll() 
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory;");
+	}
+
+	public function save() 
+	{
+		$sql = new Sql();
+
+		$r = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+			":idcategory"=>$this->getidcategory(),
+			":descategory"=>$this->getdescategory()
+		));
+
+		$this->setData($r[0]);
+	}
+
+	public function get($idcategory)
+	{
+		$sql = new Sql();
+
+		$r = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory;", [
+			":idcategory"=>$idcategory
+		]);
+
+		$this->setData($r[0]);
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory;", [
+			":idcategory"=>$this->getidcategory()
+		]);
+	}
+}
