7302b2536c539a9d367c6a5d6150ce64ff4c2b6d
CostaMatues <costa.mack95@gmail.com>
2018-03-02 11:48:39 -0300

    Carrinho Calculo de frete

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -10,6 +10,7 @@ use \Hcode\Model\User;
 class Cart extends Model 
 {	
 	const SESSION = "Cart";
+	const SESSION_ERROR = "CartError";
 
 	/**
 	 * 
@@ -79,7 +80,7 @@ class Cart extends Model
 	}
 
 	/**
-	 * 
+	 * Retorna o carrinho
 	 * @param int $idcart 
 	 * @return type
 	 */
@@ -118,7 +119,7 @@ class Cart extends Model
 	}
 
 	/**
-	 * Adiciona um produto ao carrinho
+	 * Adiciona produto ao carrinho
 	 * @param Product $product 
 	 * @return type
 	 */
@@ -130,10 +131,12 @@ class Cart extends Model
 			":idcart"=>$this->getidcart(),
 			":idproduct"=>$product->getidproduct()
 		]);
+
+		$this->getCalculateTotal();
 	}
 
 	/**
-	 * 
+	 * Remove produto do carrinho
 	 * @param Product $product 
 	 * @param type|bool $all 
 	 * @return type
@@ -156,10 +159,12 @@ class Cart extends Model
 				":idproduct"=>$product->getidproduct()
 			]);
 		}
+
+		$this->getCalculateTotal();
 	}
 
 	/**
-	 * 
+	 * Retorna os produtos do carrinho
 	 * @return type
 	 */
 	public function getProducts()
@@ -176,6 +181,172 @@ class Cart extends Model
 			]);
 
 		return $rs;
+	}
+
+	/**
+	 * Calcula o valor total do carrinho
+	 * @return type
+	 */
+	public function getProductsTotals() 
+	{
+		$sql = new Sql();
+
+		$r = $sql->select("
+			SELECT SUM(vlprice) AS vlprice, SUM(vlwidth) AS vlwidth, SUM(vlheight) AS vlheight, SUM(vllength) AS vllength, SUM(vlweight) AS vlweight, COUNT(*) AS nrqtd 
+			FROM tb_products a 
+			INNER JOIN tb_cartsproducts b ON a.idproduct = b.idproduct 
+			WHERE b.idcart = :idcart AND dtremoved IS NULL;", [
+				":idcart"=>$this->getidcart()
+			]);
+
+		if (count($r) > 0) 
+		{
+			return $r[0];
+		}
+		else 
+		{
+			return [];
+		}
+	}
+
+	/**
+	 * Calcula valor do frete
+	 * @param type $nrzipcode 
+	 * @return type
+	 */
+	public function setFreight($nrzipcode)
+	{
+		$nrzipcode = str_replace('-', '', $nrzipcode);
+
+		$totals = $this->getProductsTotals();
+
+		if ($totals['nrqtd'] > 0)
+		{
+			if ($totals['vlheight'] < 2) $totals['vlheight'] = 2;
+
+			if ($totals['vllength'] < 16) $totals['vllength'] = 16;
+			
+			$qs = http_build_query([
+				"nCdEmpresa"=>"",
+				"sDsSenha"=>"",
+				"nCdServico"=>"40010",
+				"sCepOrigem"=>"01224001",
+				"sCepDestino"=>$nrzipcode,
+				"nVlPeso"=>$totals['vlweight'],
+				"nCdFormato"=>"1",
+				"nVlComprimento"=>$totals['vllength'],
+				"nVlAltura"=>$totals['vlheight'],
+				"nVlLargura"=>$totals['vlwidth'],
+				"nVlDiametro"=>"0",
+				"sCdMaoPropria"=>"S",
+				"nVlValorDeclarado"=>$totals['vlprice'],
+				"sCdAvisoRecebimento"=>"S"
+			]);
+
+			$xml = simplexml_load_file("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo?".$qs);
+
+			$r = $xml->Servicos->cServico;
+			
+			if ($r->MsgErro != "")
+			{
+				Cart::setMsgError($r->MsgErro);
+			}
+			else
+			{
+				Cart::clearMsgError();
+			}
+
+			$this->setnrdays($r->PrazoEntrega);
+			$this->setvlfreight(Cart::formatValueToDecimal($r->Valor));
+			$this->setdeszipcode($nrzipcode);
+
+			$this->save();
+
+			return $r;
+		}
+		else 
+		{
+
+		}
+	}
+
+	/**
+	 * 
+	 * @return type
+	 */
+	public function updateFreight()
+	{
+		if ($this->getdeszipcode() != "")
+		{
+			$this->setFreight($this->getdeszipcode());
+		}
+	}
+
+
+	public function getValues()
+	{
+		$this->getCalculateTotal();
+
+		return parent::getValues();
+	}
+
+	public function getCalculateTotal()
+	{
+
+		$this->updateFreight();
+
+		$totals = $this->getProductsTotals();
+
+		$this->setvlsubtotal($totals['vlprice']);
+		
+		$this->setvltotal($totals['vlprice'] + $this->getvlfreight());
+
+	}
+
+
+
+	/**
+	 * Formata valor do frete retornado pelo webservice dos Correios
+	 * @param type $value 
+	 * @return type
+	 */
+	public static function formatValueToDecimal($value):float
+	{
+		$value = str_replace('.', '', $value);
+
+		return str_replace(',', '.', $value);
+	}
+
+	/**
+	 * Altera mensagem de erro da constante
+	 * @param type $msg 
+	 * @return type
+	 */
+	public static function setMsgError($msg)
+	{
+		$_SESSION[Cart::SESSION_ERROR] = $msg;
+	}
+
+	/**
+	 * Retorna mensagem de erro que está na constante
+	 * @return type
+	 */
+	public static function getMsgError()
+	{
+		$msg = (isset($_SESSION[Cart::SESSION_ERROR])) ? $_SESSION[Cart::SESSION_ERROR] : "";
+
+		Cart::clearMsgError();
+
+		return $msg;
+	}
+
+	/**
+	 * Apaga mensagem de erro da constante
+	 * @return type
+	 */
+	public static function clearMsgError()
+	{
+		$_SESSION[Cart::SESSION_ERROR] = NULL;
 	}
 }
 
